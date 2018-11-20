5d963a714c047e38fbe8c1f7c1b7766041381520
Felpel <jerefelp@donegalgroup.com>
2018-11-20 09:21:58 -0500

    Added "VMWare Related" directory and "Deploy-VM" script

---
+++ VMWare-Scripts/Deploy-VM.ps1
@@ -0,0 +1,16 @@
+<#
+.Synopsis
+Deploys a VM based on some basic input from the user and some assumptions about the vmware environment.
+
+.Description
+
+.Notes
+Written by Jeremy Felpel
+GitHub: jfelpel
+
+.Link
+https://github.com/jfelpel/PowerShell-StandaloneScripts
+
+.Example
+
+#>
