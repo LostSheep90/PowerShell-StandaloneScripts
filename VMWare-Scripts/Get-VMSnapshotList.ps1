870347e2e4c55dbf79b6da722db8260980cbc7bd
Felpel <jerefelp@donegalgroup.com>
2018-12-19 13:56:29 -0500

    Copied script from archive

--- VMWare-Scripts/Get-VMSnapshotList.ps1
+++ VMWare-Scripts/Get-VMSnapshotList.ps1
@@ -1,4 +1,10 @@
-Connect-VIServer -Server dmi-win-vcenter.donegalgroup.com
+Param(
+	[parameter(Mandatory=$false)]
+	[String]
+	$VCenterServer = 'dmi-win-vcenter.donegalgroup.com'
+)
+
+Connect-VIServer -Server $VCenterServer
 
 get-vm * | Get-Snapshot | Select-Object VM, Name, Created, Description, SizeGB | Out-GridView -Title "VMs with Snapshots" -PassThru
 
